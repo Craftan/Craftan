1edb9aa67cb4a4d0a70caef8c7ba7c4f358e7af7
Devin <devinfritz@gmx.de>
2024-08-19 19:45:19 +0200

    Fixed event system [maybe]

--- src/main/kotlin/de/craftan/game/CraftanGameEvents.kt
+++ src/main/kotlin/de/craftan/game/CraftanGameEvents.kt
@@ -1,18 +1,55 @@
 package de.craftan.game
 
-import de.craftan.events.GenericEventHandler
+import de.craftan.events.EventParameters
 
 /**
  * Includes all the events a craftan game has
  * @see CraftanGame
  */
-abstract class CraftanGameEvents : GenericEventHandler<CraftanGame>() {
-    fun onStart(block: CraftanGame.() -> Unit) = registerEvent(CraftanEvent.GAME_START.key, block)
+abstract class CraftanGameEvents {
+    private val eventMap = mutableMapOf<CraftanEventEnum, MutableList<CraftanGame.(event: CraftanEvent<out EventParameters>) -> Unit>>()
 
-    fun onEnd(block: CraftanGame.() -> Unit) = registerEvent(CraftanEvent.GAME_END.key, block)
+    fun onStart(block: CraftanGame.(event: CraftanEvent<EventParameters>) -> Unit) {
+        @Suppress("UNCHECKED_CAST")
+        addEventListener(block as CraftanGame.(CraftanEvent<out EventParameters>) -> Unit, CraftanEventEnum.GAME_START)
+    }
+
+    fun onEnd(block: CraftanGame.(event: CraftanEvent<EventParameters>) -> Unit) {
+        @Suppress("UNCHECKED_CAST")
+        addEventListener(block as CraftanGame.(CraftanEvent<out EventParameters>) -> Unit, CraftanEventEnum.GAME_END)
+    }
+
+    private fun addEventListener(
+        listener: CraftanGame.(event: CraftanEvent<out EventParameters>) -> Unit,
+        event: CraftanEventEnum,
+    ) {
+        val list = eventMap[event]
+
+        if (list == null) {
+            eventMap[event] = mutableListOf()
+        }
+
+        eventMap[event]?.add(listener) ?: error("This should not be null lol")
+    }
+
+    fun triggerEvent(
+        eventMapping: CraftanEventEnum,
+        game: CraftanGame,
+        parameters: CraftanEvent<out EventParameters>,
+    ) {
+        eventMap[eventMapping]?.forEach { it(game, parameters) }
+    }
 }
 
-enum class CraftanEvent(
+open class CraftanEvent<P : EventParameters>(
+    val event: CraftanEventEnum,
+)
+
+data class GameStartParameters(
+    val idk: Int,
+) : EventParameters()
+
+enum class CraftanEventEnum(
     val key: String,
 ) {
     GAME_START("start"),
