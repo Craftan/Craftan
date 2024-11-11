7508a557deb76c20305c04686f1979fe0dd25084
devinfritz <devinfritz@gmx.de>
2024-11-11 16:10:34 +0100

    Fixed command cascading, and permission generation.

--- src/main/kotlin/de/craftan/io/commands/CraftanCommand.kt
+++ src/main/kotlin/de/craftan/io/commands/CraftanCommand.kt
@@ -26,29 +26,14 @@ fun craftanCommand(
 ): LiteralArgumentBuilder<CommandSourceStack> {
     val permission = "craftan.cmd.$name"
     val command =
-        command(name, builder = builder).apply {
+        command(name) {
             requiresPermission(permission)
-
-            literal("info") {
-                runs {
-                    val commandArgs = arguments.map { it.usageText }
-                    val information =
-                        CraftanNotification.INFORMATION_FORMATING
-                            .resolveWithPlaceholder(
-                                player,
-                                mapOf(
-                                    CraftanPlaceholder.COMMAND_ARGS to commandArgs.joinToString(" "),
-                                    CraftanPlaceholder.COMMAND_NAME to name,
-                                    CraftanPlaceholder.COMMAND_DESCRIPTION to description,
-                                ),
-                            )
-
-                    player.sendMessage(information)
-                }
-            }
+            infoSubCommand(name, description)
         }
 
     CommandPermissionUtil.commandPermissions[command] = CraftanCommand(name, description, permission)
+    command.apply(builder)
+
     return command
 }
 
@@ -58,16 +43,46 @@ inline fun ArgumentBuilder<CommandSourceStack, *>.craftanSubCommand(
     builder: LiteralArgumentBuilder<CommandSourceStack>.() -> Unit = {},
 ) {
     val parent = CommandPermissionUtil.commandPermissions[this] ?: error("Parent command not found. Use <craftanCommand()> to build a craftan sub command!")
+
+    val computedName = "${parent.name} $name"
+
     val permission = parent.permission + ".$name"
 
     val subCommand =
-        command(name, false, builder).apply {
+        command(name, false) {
             requiresPermission(permission)
+            infoSubCommand(computedName, description)
         }
-    then(subCommand)
+
     val cc = CraftanCommand(name, description, permission, parent)
-    CommandPermissionUtil.commandPermissions[this] = cc
+    CommandPermissionUtil.commandPermissions[subCommand] = cc
+
+    subCommand.apply(builder)
+    then(subCommand)
 }
+
+inline fun ArgumentBuilder<CommandSourceStack, *>.infoSubCommand(
+    name: String,
+    description: String,
+    builder: LiteralArgumentBuilder<CommandSourceStack>.() -> Unit = {},
+) = command("info", false, builder)
+    .apply {
+        runs {
+            val commandArgs = arguments.map { it.usageText }
+            val information =
+                CraftanNotification.INFORMATION_FORMATING
+                    .resolveWithPlaceholder(
+                        player,
+                        mapOf(
+                            CraftanPlaceholder.COMMAND_ARGS to commandArgs.joinToString(" "),
+                            CraftanPlaceholder.COMMAND_NAME to name,
+                            CraftanPlaceholder.COMMAND_DESCRIPTION to description,
+                        ),
+                    )
+
+            player.sendMessage(information)
+        }
+    }.also { then(it) }
 
 data class CraftanCommand(
     val name: String,
