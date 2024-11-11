73ebeeef6dabdf093a063af592a0ac1e6f771278
Devin <devinfritz@gmx.de>
2024-11-11 18:58:49 +0100

    Fixed compilation issues
    Signed-off-by: devinfritz <devinfritz@gmx.de>

--- src/main/kotlin/de/craftan/Craftan.kt
+++ src/main/kotlin/de/craftan/Craftan.kt
@@ -2,14 +2,12 @@ package de.craftan
 
 import de.craftan.commands.craftanCommand
 import de.craftan.commands.structureCommand
-import de.craftan.commands.testCommand
 import de.craftan.config.ConfigSystem
 import de.craftan.io.MessageAdapter
 import de.craftan.util.CraftanSystem
 import de.craftan.util.SystemManager
 import java.io.File
 import java.nio.file.Files
-import kotlin.io.path.exists
 
 object Craftan {
     val schematicsFolder = File("${PluginManager.dataFolder.absolutePath}/schematics")
@@ -51,6 +49,5 @@ object Craftan {
     private fun loadCommands() {
         structureCommand
         craftanCommand
-        testCommand
     }
 }
