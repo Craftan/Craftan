c939d6bd1facb499b07deef3b0de067fc6b3de7c
devinfritz <devinfritz@gmx.de>
2025-01-22 18:16:42 +0100

    Changed action to accept dto
    Signed-off-by: devinfritz <devinfritz@gmx.de>

--- src/main/kotlin/de/craftan/engine/CraftanGameAction.kt
+++ src/main/kotlin/de/craftan/engine/CraftanGameAction.kt
@@ -1,8 +1,6 @@
-package de.craftan
+package de.craftan.engine
 
-import de.craftan.engine.CraftanEvent
-import de.craftan.engine.CraftanGame
-import de.craftan.engine.CraftanPlayer
+import de.craftan.config.CraftanConfig
 import de.craftan.engine.gameflow.CraftanActionItem
 import de.staticred.kia.inventory.item.KItem
 import net.ormr.eventbus.EventBus
@@ -25,13 +23,12 @@ interface CraftanGameAction<R> {
 
     /**
      * Executes this given action with the given player
-     *
-     * This should directly modify the supplied game object
+     * Should fire an event for the game to handle the changed state
      *
      * @param player the player who invoked this action
      * @return whether the action was successful or not
      */
-    fun invoke(player: CraftanPlayer): Boolean
+    fun invoke(player: CraftanPlayer, data: CraftanActionData): Boolean
 
     /**
      * Builds this action as a KItem
@@ -40,3 +37,16 @@ interface CraftanGameAction<R> {
      */
     fun asItem(): CraftanActionItem<R>
 }
+
+/**
+ * Models a data object to be shared between the ingame and the engine
+ */
+interface CraftanActionData {
+    companion object {
+        fun empty(): CraftanActionData {
+            return EmptyCraftanActionData()
+        }
+    }
+}
+
+class EmptyCraftanActionData: CraftanActionData
