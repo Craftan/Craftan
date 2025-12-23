cc7fc92bb9f0a2e8f4f0c374e67be3d059a0c3d4
devin <devinfritz@gmx.de>
2025-12-23 17:24:33 +0100

    Remove SampleConfig system and integrate section annotations into CraftanConfig

--- src/main/kotlin/de/craftan/config/CraftanConfig.kt
+++ src/main/kotlin/de/craftan/config/CraftanConfig.kt
@@ -3,6 +3,7 @@ package de.craftan.config
 import de.craftan.io.config.CraftanFileConfig
 import de.craftan.io.config.Location
 import de.craftan.io.config.ConfigPath
+import de.craftan.io.config.Section
 
 @ConfigPath("config/craftan.yml")
 data class CraftanConfig(
@@ -15,6 +16,7 @@ data class CraftanConfig(
     val timeToDice: Int = 60,
 
     @Location("game.defaults")
+    @Section("game.defaults", "Default game-related values")
     val pointsToWin: Int = 7,
 
 ) : CraftanFileConfig
